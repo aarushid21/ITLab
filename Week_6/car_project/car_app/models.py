125f79929b010c622e6bb3cc94893c77ae712aff
Aarushi Anuj Dharna <99038418+aarushid21@users.noreply.github.com>
2024-02-26 16:10:46 +0530

    Delete Week_6/car_project directory

--- Week_6/car_project/car_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Car(models.Model):
-    manufacturer = models.CharField(max_length=100)
-    model_name = models.CharField(max_length=100)
--- Week_6/car_project/car_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from car_app import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('car_detail/<str:manufacturer>/<str:model_name>/', views.car_detail, name='car_detail'),
-]
